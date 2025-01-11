07ef47ee203a9504b69fc9dc773fba1a7d44f184
Subhomoy Das <subhomoydas54@gmail.com>
2025-01-11 21:43:34 +0530

    For checking purpose

--- src/Arrays/SumOfElements.java
+++ src/Arrays/SumOfElements.java
@@ -1,2 +1,15 @@
-package Arrays;public class SumOfElements {
+package Arrays;
+
+public class SumOfElements {
+    static  int sum(int arr[] , int n){
+        int s = 0;
+        for (int i = 0; i < arr.length; i++) {
+            s = s + arr[i];
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,3,6,5};
+        System.out.println(sum(arr,4));
+    }
 }
